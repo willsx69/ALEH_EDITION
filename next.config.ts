06d16fe44c38a46a8a742e0f9fa9990dc59e32dd
william <williambreno666@gmail.com>
2026-07-23 01:13:14 -0300

    Remove old page.tsx, update config and styles

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+  },
 };
 
 export default nextConfig;
